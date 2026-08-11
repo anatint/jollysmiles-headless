8fc8266452ffdfbc1fded78d490d620d9cb5199d
Jolly Smiles <deploy@jollysmiles.com>
2026-08-11 11:38:56 +0530

    Fix build: Remove unused Image import from Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { MapPin, Phone, Mail, Clock, Facebook, Instagram, Twitter } from 'lucide-react';
 import Link from 'next/link';
-import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
 export default function Footer() {
