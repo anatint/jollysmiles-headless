a02cf91e0b57b5142850aeee543137fc03325f0f
anatint <anatint@users.noreply.github.com>
2026-09-01 12:24:49 +0530

    add deploy.bat script

---
+++ deploy.bat
@@ -0,0 +1,10 @@
+@echo off
+echo ==========================================
+echo Deploying Jolly Smiles to Cloudflare Workers...
+echo ==========================================
+call npm run deploy
+echo.
+echo ==========================================
+echo DEPLOYMENT COMPLETE!
+echo ==========================================
+pause
